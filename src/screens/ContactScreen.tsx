dc945194f06a8513e432da8e338783f0567cb8d6
rushclin02 <takamrushclin@gmail.com>
2023-02-05 06:46:22 +0100

    Install and Test MUI-DataGrid

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,5 +1,86 @@
+import styled from "@emotion/styled";
+import { Button, Divider, Grid, Paper, Typography } from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import React from "react";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
 const ContactScreen = () => {
-  return <h1>Yoo</h1>;
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          backgroundColor: "#ccc",
+          maxWidth: "80%",
+          marginTop: "20px",
+          textAlign: "center",
+          padding: "20px",
+          borderRadius: "7px",
+        }}
+      >
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={5}
+        >
+          <Typography variant="h4" fontWeight={800}>
+            Kontak.
+          </Typography>
+          <Button variant="contained">New contact</Button>
+        </Stack>
+        <Divider light />
+
+        <Box
+          sx={{
+            height: "390px",
+          }}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[6]}
+            checkboxSelection
+          />
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 };
 
 export default ContactScreen;
